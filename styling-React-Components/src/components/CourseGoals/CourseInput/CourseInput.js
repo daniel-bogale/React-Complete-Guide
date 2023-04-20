c852b8bfe0b63a887b389ef685cc757670562b11
danibog <dannybg090909@gmail.com>
2023-04-20 00:27:45 -0700

    working on styled comonent button and courseInput is styled

--- styling-React-Components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ styling-React-Components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,44 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+// import "./CourseInput.css";
+
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  margin: 0.5rem 0;
+  transition: all 1s ease-out;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background-color: #ffd7d7;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -27,14 +64,14 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? " invalid" : ""}`}>
+      <StyledDiv className={!isValid && " invalid"}>
         <label>Course Goal</label>
         <input
           onFocus={onFocusHandler}
           type="text"
           onChange={goalInputChangeHandler}
         />
-      </div>
+      </StyledDiv>
       <Button type="submit">Add Goal</Button>
     </form>
   );
