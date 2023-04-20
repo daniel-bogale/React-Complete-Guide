209da60b318fd0e5024663110f169c368cd1d1f4
danibog <dannybg090909@gmail.com>
2023-04-20 02:13:06 -0700

    button component is styled using css modules

--- styling-React-Components/src/components/UI/Button/Button.js
+++ styling-React-Components/src/components/UI/Button/Button.js
@@ -1,4 +1,6 @@
-import styled from "styled-components";
+// import styled from "styled-components";
+
+import styles from "./Button.module.css";
 
 // import './Button.css';
 
@@ -10,25 +12,38 @@ import styled from "styled-components";
 //   );
 // };
 
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
-  $:focus {
-    outline: none;
-  }
+// const Button = styled.button`
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  $:hover,
-  $:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   width: 100%;
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+
+//   $:focus {
+//     outline: none;
+//   }
+
+//   $:hover,
+//   $:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
 export default Button;
