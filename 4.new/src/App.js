801850c30adabb449779a94c5dd2d7dfa208b3c5
danibog <dannybg090909@gmail.com>
2023-04-21 14:54:59 -0700

    persist logged in after refresh is built

---
+++ 4.new/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedLoggedInVal = localStorage.getItem("isLoggedIn");
+    if (storedLoggedInVal === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "1");
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.setItem("isLoggedIn", "0");
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
