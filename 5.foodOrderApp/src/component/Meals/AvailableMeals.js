099e18ff3e98f1bb2fec416e68c05906791c816b
danibog <dannybg090909@gmail.com>
2023-05-06 12:08:39 -0700

    error handling is built

--- 5.foodOrderApp/src/component/Meals/AvailableMeals.js
+++ 5.foodOrderApp/src/component/Meals/AvailableMeals.js
@@ -31,31 +31,57 @@ import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
-  const [loadingMeals, setLoadingMeals] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
   useEffect(() => {
     const fetchMeals = async () => {
-      setLoadingMeals(true);
-      const response = await fetch(
-        "https://react-first-38e92-default-rtdb.firebaseio.com/meals.json"
-      );
+      try {
+        const response = await fetch(
+          "https://react-first-38e92-default-rtdb.firebaseio.com/meals.json"
+        );
 
-      const responseData = await response.json();
-      const loadedMeals = [];
-      for (const key in responseData) {
-        loadedMeals.push({
-          id: key,
-          name: responseData[key].name,
-          description: responseData[key].description,
-          price: responseData[key].price,
-        });
+        if (!response.ok) {
+          console.log("error");
+          throw new Error("Something went wrong");
+        }
+
+        const responseData = await response.json();
+        const loadedMeals = [];
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price,
+          });
+        }
+
+        setMeals(loadedMeals);
+      } catch (error) {
+        setHttpError(error.message);
       }
-
-      setMeals(loadedMeals);
     };
+
     fetchMeals();
-    setLoadingMeals(false);
+
+    setIsLoading(false);
   }, []);
 
+  if (httpError) {
+    return (
+      <section className={styles["meals-error"]}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <section className={styles["meals-loading"]}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   const mealsList = meals.map((meal) => (
     <MealItem
       name={meal.name}
@@ -67,11 +93,11 @@ const AvailableMeals = () => {
       {meal.name}
     </MealItem>
   ));
+
   return (
     <section className={styles.meals}>
       <Card>
-        {!loadingMeals && <ul>{mealsList}</ul>}
-        {loadingMeals && "loading..."}
+        <ul>{mealsList}</ul>
       </Card>
     </section>
   );
