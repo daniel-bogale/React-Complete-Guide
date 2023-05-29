37dfd2569e59d357ea5758b6043ad8ea223fe281
danibog <dannybg090909@gmail.com>
2023-05-29 20:29:34 +0200

    folder is organised

--- 15.router-practice/src/components/comments/Comments.js
+++ 15.router-practice/src/components/comments/Comments.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
@@ -9,12 +9,12 @@ const Comments = () => {
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           Add a Comment
         </button>
       )}
