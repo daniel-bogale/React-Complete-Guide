f3565af49cd719423279aab6254906f0fa9dabd3
danibog <dannybg090909@gmail.com>
2023-05-31 16:35:50 +0200

    on fetch fail is done

--- 16.authentication/src/components/Auth/AuthForm.js
+++ 16.authentication/src/components/Auth/AuthForm.js
@@ -44,7 +44,7 @@ const AuthForm = () => {
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="email">Your Email</label>
           <input type="email" id="email" required ref={emailInputRef} />
