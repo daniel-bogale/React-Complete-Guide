e23cbb510355590d9c95a6717bb4b067f177d315
danibog <dannybg090909@gmail.com>
2023-06-08 21:25:15 +0200

    the second test is done

--- 19.testing-react-app/src/components/Greeting.js
+++ 19.testing-react-app/src/components/Greeting.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState();
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
   return (
     <div>
       <h2>Hello</h2>
-      <p>It's good to see you!</p>
+      {!changedText && <p>It's good to see you!</p>}
+      {changedText && <p>Changed!</p>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
 };
+
 export default Greeting;
--- 19.testing-react-app/src/components/Greeting.test.js
+++ 19.testing-react-app/src/components/Greeting.test.js
@@ -1,11 +1,32 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("render hello as a text ", () => {
+describe("Greeting component", () => {
+  test("render hello as a text ", () => {
+    //Arrange
+    render(<Greeting />);
+    //Act
+    //Assert
+    const helloElem = screen.getByText(/hello/i);
+    expect(helloElem).toBeInTheDocument();
+  });
+});
+
+test("renders good to see you if the button was NOT clicked", () => {
+  render(<Greeting />);
+  const outputElement = screen.getByText("good to see you", { exact: false });
+  expect(outputElement).toBeInTheDocument();
+});
+
+test("render 'changed!' if the button was clicked", () => {
   //Arrange
   render(<Greeting />);
+
   //Act
+  const buttonElement = screen.getByRole("button");
+  userEvent.click(buttonElement);
+
   //Assert
-  const helloElem = screen.getByText(/hello/i);
-  expect(helloElem).toBeInTheDocument();
+  const outputElement = screen.getByText(" ", { exact: false });
+  expect(outputElement).toBeInTheDocument();
 });
