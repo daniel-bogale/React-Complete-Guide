a1b5173c4a137d7e9d88631fa7785672812d49b9
danibog <dannybg090909@gmail.com>
2023-06-22 14:20:04 +0300

    some error is fixed

--- 5.foodOrderApp/src/component/Cart/Checkout.js
+++ 5.foodOrderApp/src/component/Cart/Checkout.js
@@ -9,7 +9,7 @@ const Checkout = (props) => {
     name: true,
     id: true,
     city: true,
-    postalCode: true,
+    blockNumber: true,
   });
   const nameInputRef = useRef();
   const idInputRef = useRef();
@@ -20,23 +20,23 @@ const Checkout = (props) => {
     event.preventDefault();
     const enteredName = nameInputRef.current.value;
     const enteredId = idInputRef.current.value;
-    const enteredPostal = postalInputRef.current.value;
+    const enteredBlockNumber = postalInputRef.current.value;
     const enteredCity = cityInputRef.current.value;
     const enteredNameIsValid = !isEmpty(enteredName);
     const enteredIdIsValid = !isEmpty(enteredId);
     const enteredCityIsValid = !isEmpty(enteredCity);
-    const enteredPostalIsValid = isFiveChars(enteredPostal);
+    const enteredBlockNumberIsValid = isFiveChars(enteredBlockNumber);
 
     const formIsValid =
       enteredNameIsValid &&
       enteredCityIsValid &&
-      enteredPostalIsValid &&
+      enteredBlockNumberIsValid &&
       enteredIdIsValid;
     setFormInputsValidity({
       name: enteredNameIsValid,
       id: enteredIdIsValid,
       city: enteredCityIsValid,
-      postalCode: enteredPostalIsValid,
+      blockNumber: enteredBlockNumberIsValid,
     });
     if (!formIsValid) {
       return;
@@ -45,7 +45,7 @@ const Checkout = (props) => {
       name: enteredName,
       id: enteredId,
       city: enteredCity,
-      postalCode: enteredPostal,
+      blockNumber: enteredBlockNumber,
     });
   };
 
@@ -55,8 +55,8 @@ const Checkout = (props) => {
   const idInputClass = `${classes.control} ${
     formInputsValidity.id ? "" : classes.invalid
   }`;
-  const postalInputClass = `${classes.control} ${
-    formInputsValidity.postalCode ? "" : classes.invalid
+  const blockNumberInputClass = `${classes.control} ${
+    formInputsValidity.blockNumber ? "" : classes.invalid
   }`;
   const cityInputClass = `${classes.control} ${
     formInputsValidity.city ? "" : classes.invalid
@@ -75,27 +75,20 @@ const Checkout = (props) => {
       </div>
       <div className={idInputClass}>
         <label htmlFor="id">Id Number</label>
-        <input
-          type="text"
-          id="id"
-          placeholder="ETS-1234/12"
-          value="ETS-"
-          ref={idInputRef}
-        />
+        <input type="text" id="id" placeholder="ETS-1234/12" ref={idInputRef} />
         {!formInputsValidity.id && (
           <p className="invalid-message">Please enter a valid id!</p>
         )}
       </div>
-      <div className={postalInputClass}>
-        <label htmlFor="postal">Block Number</label>
+      <div className={blockNumberInputClass}>
+        <label htmlFor="blockNumber">Block Number</label>
         <input
           type="text"
-          id="postal"
-          value="B-"
-          ref={postalInputRef}
+          id="blockNumber"
+          ref={blockNumberInputRef}
           placeholder="B12"
         />
-        {!formInputsValidity.postalCode && (
+        {!formInputsValidity.blockNumber && (
           <p className="invalid-message">Please enter a valid Block Number!</p>
         )}
       </div>
